17bb544e8d06a9715244dbebca09327cd830a05e
Jetroid <holtjet@gmail.com>
2016-09-12 18:20:46 +0100

    Set up function

---
+++ bigtext.js
@@ -0,0 +1,17 @@
+HTMLDivElement.prototype.bigText = function(options){
+	if (typeof options === 'undefined') { 
+		options = {
+			rotateText: null,
+			fontSizeFactor: 0.8,
+			maximumFontSize: null,
+			limitingDimension: "both",
+			horizontalAlign: "center",
+			verticalAlign: "center",
+			textAlign: "center",
+			whiteSpace: "nowrap"
+		};
+	}
+	
+	
+	
+}
