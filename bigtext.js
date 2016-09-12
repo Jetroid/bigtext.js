38e799eb71995c8b7a80a4180bf3de4f1dbafa72
Jetroid <holtjet@gmail.com>
2016-09-12 21:53:31 +0100

    Complete conversion

--- bigtext.js
+++ bigtext.js
@@ -1,3 +1,35 @@
+calculateInnerDimensions = function(computedStyle){
+	//Calculate the parent inner width and height
+	//If box-sizing is border-box, we need to subtract padding and border.
+	var innerWidth;
+	var	innerHeight;
+
+	var width = parseInt(computedStyle.getPropertyValue("width"));
+	var height = parseInt(computedStyle.getPropertyValue("height"));
+	var paddingLeft = parseInt(computedStyle.getPropertyValue("padding-left"));
+	var paddingRight = parseInt(computedStyle.getPropertyValue("padding-right"));
+	var paddingTop = parseInt(computedStyle.getPropertyValue("padding-top"));
+	var paddingBottom = parseInt(computedStyle.getPropertyValue("padding-bottom"));
+	var borderLeft = parseInt(computedStyle.getPropertyValue("border-left"));
+	var borderRight = parseInt(computedStyle.getPropertyValue("border-right"));
+	var borderTop = parseInt(computedStyle.getPropertyValue("border-top"));
+	var borderBottom = parseInt(computedStyle.getPropertyValue("border-right"));
+
+	//If box-sizing is border-box, we need to subtract padding and border.
+	var parentBoxSizing = computedStyle.getPropertyValue("box-sizing");
+	if(parentBoxSizing == "border-box"){
+		innerWidth = width - (paddingLeft + paddingRight + borderLeft + borderRight);
+		innerHeight = height - (paddingTop + paddingBottom + borderTop + borderBottom);
+	}else{
+		innerWidth = width;
+		innerHeight = height;
+	}
+	var obj = {};
+	obj["width"] = innerWidth;
+	obj["height"] = innerHeight;
+	return obj;
+}
+
 HTMLDivElement.prototype.bigText = function(options){
 	var defaultOptions = {
 		rotateText: null,
@@ -45,10 +77,102 @@ HTMLDivElement.prototype.bigText = function(options){
 		bottom: parseInt(parentComputedStyle.getPropertyValue("padding-bottom")),
 		left: parseInt(parentComputedStyle.getPropertyValue("padding-left")),
 	};
+
+	var parentBorder = {
+		top: parseInt(parentComputedStyle.getPropertyValue("border-top")),
+		right: parseInt(parentComputedStyle.getPropertyValue("border-right")),
+		bottom: parseInt(parentComputedStyle.getPropertyValue("border-bottom")),
+		left: parseInt(parentComputedStyle.getPropertyValue("border-left")),
+	};
+
+	//Calculate the parent inner width and height
+	var parentInnerDimensions = calculateInnerDimensions(parentComputedStyle);
+	var parentInnerWidth = parentInnerDimensions["width"];
+	var parentInnerHeight = parentInnerDimensions["height"];
 	
 	var box = {
 		width: this.offsetWidth, //Note: This is slightly larger than the jQuery version
 		height: this.offsetHeight,
 	};
+
+	if (options.rotateText !== null) {
+		if (typeof options.rotateText !== "number")
+			throw "bigText error: rotateText value must be a number";
+		var rotate= "rotate(" + options.rotateText + "deg)";
+		style.webkitTransform = rotate;
+		style.msTransform = rotate;
+		style.MozTransform = rotate;
+		style.OTransform = rotate;
+		style.transform = rotate;
+		//calculating bounding box of the rotated element
+		var sine = Math.abs(Math.sin(options.rotateText * Math.PI / 180));
+		var cosine = Math.abs(Math.cos(options.rotateText * Math.PI / 180));
+		box.width = this.offsetWidth * cosine + this.offsetHeight * sine;
+		box.height = this.offsetWidth * sine + this.offsetHeight * cosine;
+	}
 	
+	var widthFactor = (parentInnerWidth - parentPadding.left - parentPadding.right) / box.width;
+	var heightFactor = (parentInnerHeight - parentPadding.top - parentPadding.bottom) / box.height;
+	var lineHeight;
+
+	if (options.limitingDimension.toLowerCase() === "width") {
+		lineHeight= Math.floor(widthFactor * 1000);
+		parentStyle.height = lineHeight;
+	} else if (options.limitingDimension.toLowerCase() === "height") {
+		lineHeight= Math.floor(heightFactor * 1000);
+	} else if (widthFactor < heightFactor)
+		lineHeight= Math.floor(widthFactor * 1000);
+	else if (widthFactor >= heightFactor)
+		lineHeight= Math.floor(heightFactor * 1000);
+
+	var fontSize = lineHeight * options.fontSizeFactor;
+	if (options.maximumFontSize !== null && fontSize > options.maximumFontSize) {
+		fontSize = options.maximumFontSize;
+		lineHeight = fontSize / options.fontSizeFactor;
+	}
+
+	style.fontSize = Math.floor(fontSize) + "px";
+	style.lineHeight = Math.ceil(lineHeight)  + "px";
+	style.marginBottom = "0px";
+	style.marginRight = "0px";
+
+	if (options.limitingDimension.toLowerCase() === "height") {
+		//this option needs the font-size to be set already so computedStyle.getPropertyValue("width") returns the right size
+		//this +4 is to compensate the rounding erros that can occur due to the calls to Math.floor in the centering code
+		parentStyle.width = (parseInt(computedStyle.getPropertyValue("width")) + 4) + "px";
+	}
+
+	//Calculate the parent inner width and height
+	var innerDimensions = calculateInnerDimensions(computedStyle);
+	var innerWidth = innerDimensions["width"];
+	var innerHeight = innerDimensions["height"];
+
+	switch(options.verticalAlign.toLowerCase()) {
+		case "top":
+			style.top = "0%";
+		break;
+		case "bottom":
+			style.top = "100%";
+			style.marginTop = Math.floor(-innerHeight) + "px";
+		break;
+		default:
+			style.marginTop = Math.floor((-innerHeight / 2)) + "px";
+		break;
+	}
+
+	switch(options.horizontalAlign.toLowerCase()) {
+		case "left":
+			style.left = "0%";
+		break;
+		case "right":
+			style.left = "100%";
+			style.marginTop = Math.floor(-innerWidth) + "px";
+		break;
+		default:
+			style.marginLeft = Math.floor((-innerWidth / 2)) + "px";
+		break;
+	}
+
+	//shows the element after the work is done
+	style.visibility = "visible";
 }
